b8893a28b12eebe51320fc1c01983f9287804570
Pratyush Fekarikar <pratyush.f@venhan.com>
2025-03-08 22:39:39 +0530

    Started with Swiggy card mapping

--- src/Components/FoodItems.jsx
+++ src/Components/FoodItems.jsx
@@ -2,31 +2,69 @@ import {
   FaRegArrowAltCircleLeft,
   FaRegArrowAltCircleRight,
 } from "react-icons/fa";
+import { useState } from "react";
+import { data } from "../Data/data";
+
+const baseImageUrl =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
 
 const FoodItems = () => {
-  const arr1 = [1, 2, 3, 4, 5, 6, 7,5];
+  const [startIndex, setStartIndex] = useState(0);
+  const itemsPerPage = 5; // Number of visible items
+  const shiftValue = 3; // Number of items to shift per click
+  const maxIndex = data.length - itemsPerPage; // Prevent overflow
+
+  const handleNext = () => {
+    if (startIndex + shiftValue <= maxIndex) {
+      setStartIndex(startIndex + shiftValue);
+    } else {
+      setStartIndex(maxIndex); // Stop at last page
+    }
+  };
+
+  const handlePrev = () => {
+    if (startIndex - shiftValue >= 0) {
+      setStartIndex(startIndex - shiftValue);
+    } else {
+      setStartIndex(0); // Stop at first page
+    }
+  };
+
   return (
     <div className="flex p-1 flex-col h-full w-[80%] gap-5 border-b-2 border-b-gray-200">
-      <div className=" flex justify-between">
-        <p className=" p-2 text-2xl font-semibold ">
-          Pratyush whats on your mind ?
-        </p>
-        <div className=" flex items-center text-3xl gap-2">
-          <FaRegArrowAltCircleLeft className=" cursor-pointer hover:text-orange-600" />
-          <FaRegArrowAltCircleRight className=" cursor-pointer hover:text-orange-600" />
+      <div className="flex justify-between">
+        <p className="p-2 text-2xl font-semibold">Pratyush, what's on your mind?</p>
+        <div className="flex items-center text-3xl gap-2">
+          <FaRegArrowAltCircleLeft
+            onClick={handlePrev}
+            className={`cursor-pointer hover:text-orange-600 ${
+              startIndex === 0 ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          />
+          <FaRegArrowAltCircleRight
+            onClick={handleNext}
+            className={`cursor-pointer hover:text-orange-600 ${
+              startIndex >= maxIndex ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          />
         </div>
       </div>
-      <div className="flex">
-        {arr1.map((e) => {
-          return (
+
+      {/* Slider Container */}
+      <div className="overflow-hidden relative w-full">
+        <div
+          className="flex transition-transform duration-500 ease-in-out"
+          style={{ transform: `translateX(-${startIndex * (100 / itemsPerPage)}%)` }}
+        >
+          {data.map((e) => (
             <img
-              key={e}
-              className="h-40 w-40 m-4 cursor-pointer"
-              src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Biryani.png"
-              alt="foodItems"
+              key={e.id}
+              className="h-42 w-40 m-4 cursor-pointer transition-transform hover:scale-105"
+              src={`${baseImageUrl}${e.imageId}`}
+              alt={e?.action.text || "Food Item"}
             />
-          );
-        })}
+          ))}
+        </div>
       </div>
     </div>
   );
---
+++ src/Data/data.jsx
@@ -0,0 +1,377 @@
+export const data = [
+  {
+    id: "4772032",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2025/3/7/9478a99b-4fc2-4f6f-96b7-4cfda32705a2_PCTIleWday.png",
+    action: {
+      link: "https://www.swiggy.com/collections/173496?+Dessert=&collection_id=173496&header_title=Cake+&tags=layout_NY25_Page6&type=rcv2",
+      text: "Women's Day Campaign",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for women's day campaign",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=173496&tags=layout_NY25_Page6&header_title=Cake%20&%20Dessert",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "4718798",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2025/3/1/27af148a-4db6-4070-94c1-eb5c0299b978_PCTIleRamadansSpecial.png",
+    action: {
+      link: "https://www.swiggy.com/collections/170588?collection_id=170588&header_title=Ramadan+Delights&tags=layout_NY25_Page4&type=rcv2",
+      text: "Ramadan Specials",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for ramadan specials",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=170588&tags=layout_NY25_Page4&header_title=Ramadan%20Delights",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750591",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Biryani.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83639?collection_id=83639&search_context=biryani&tags=layout_CCS_Biryani&type=rcv2",
+      text: "Biryani",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for biryani",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83639&tags=layout_CCS_Biryani&search_context=biryani",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750579",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Pizzas.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83631?collection_id=83631&search_context=pizza&tags=layout_CCS_Pizza&type=rcv2",
+      text: "Pizzas",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for pizza",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83631&tags=layout_CCS_Pizza&search_context=pizza",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750589",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_burger.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83637?collection_id=83637&search_context=burger&tags=layout_CCS_Burger&type=rcv2",
+      text: "Burgers",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for burger",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83637&tags=layout_CCS_Burger&search_context=burger",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750587",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2025/1/24/897bc750-6b57-4e7d-9365-87c1ab2c6d7e_Chinese2.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83636?collection_id=83636&tags=layout_CCS_Chinese&type=rcv2",
+      text: "Chinese",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for chinese",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83636&tags=layout_CCS_Chinese",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750581",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2025/1/24/05a939eb-fd4e-4308-b989-d1c54f4421b3_northindian1.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83633?collection_id=83633&search_context=northindian&tags=layout_CCS_NorthIndian&type=rcv2",
+      text: "North Indian",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for north indian",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83633&tags=layout_CCS_NorthIndian&search_context=northindian",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "749868",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_cake.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83655?collection_id=83655&tags=layout_CCS_Cake&type=rcv2",
+      text: "Cakes",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for cake",
+      altTextCta: "open",
+    },
+    entityId: "swiggy://collectionV2?collection_id=83655&tags=layout_CCS_Cake",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750222",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/17/58760e8e-324f-479e-88fa-31800120ea38_Rolls1.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83669?collection_id=83669&tags=layout_CCS_Rolls&type=rcv2",
+      text: "Rolls",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for roll",
+      altTextCta: "open",
+    },
+    entityId: "swiggy://collectionV2?collection_id=83669&tags=layout_CCS_Rolls",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750206",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/f1263395-5d4a-4775-95dc-80ab6f3bbd89_pasta.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80479?collection_id=80479&search_context=pasta&tags=layout_CCS_Pasta&type=rcv2",
+      text: "Pasta",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for pasta",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=80479&tags=layout_CCS_Pasta&search_context=pasta",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750596",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_chocolate icecream.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83640?collection_id=83640&tags=layout_CCS_IceCreams&type=rcv2",
+      text: "Ice Cream",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for icecream",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83640&tags=layout_CCS_IceCreams",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750248",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/f1263395-5d4a-4775-95dc-80ab6f3bbd89_shawarma.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80402?collection_id=80402&tags=layout_Shawarma_Contextual&type=rcv2",
+      text: "Shawarma",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for shawarma",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=80402&tags=layout_Shawarma_Contextual",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "749772",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Noodles.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80463?collection_id=80463&tags=layout_BAU_Contextual%2Cnoodles&type=rcv2",
+      text: "Noodles",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for noodles",
+      altTextCta: "open",
+    },
+    entityId: "80463",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750201",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Paratha.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80475?collection_id=80475&tags=layout_CCS_Paratha&type=rcv2",
+      text: "Paratha",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for paratha",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=80475&tags=layout_CCS_Paratha",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "749768",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Momos.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80461?collection_id=80461&tags=layout_CCS_Momos&type=rcv2",
+      text: "Momos",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for momos",
+      altTextCta: "open",
+    },
+    entityId: "swiggy://collectionV2?collection_id=80461&tags=layout_CCS_Momos",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "749760",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Khichdi.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80455?collection_id=80455&tags=layout_BAU_Contextual%2Ckhichdi&type=rcv2",
+      text: "Khichdi",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for khichdi",
+      altTextCta: "open",
+    },
+    entityId: "80455",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750635",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/6ef07bda-b707-48ea-9b14-2594071593d1_Kebabs.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80451?collection_id=80451&tags=layout_CCS_Kebabs&type=rcv2",
+      text: "Kebabs",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for kebabs",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=80451&tags=layout_CCS_Kebabs",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750216",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_Pav Bhaji.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80362?collection_id=80362&tags=layout_PavBhaji_Contextual&type=rcv2",
+      text: "Pav Bhaji",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurant curated for pav bhaji",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=80362&tags=layout_PavBhaji_Contextual",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750571",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_Pure Veg.png",
+    action: {
+      link: "https://www.swiggy.com/collections/80435?collection_id=80435&tags=layout_CCS_PureVeg&type=rcv2",
+      text: "Pure Veg",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for veg",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=80435&tags=layout_CCS_PureVeg",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+  {
+    id: "750585",
+    imageId:
+      "MERCHANDISING_BANNERS/IMAGES/MERCH/2024/7/2/8f508de7-e0ac-4ba8-b54d-def9db98959e_Salad-1.png",
+    action: {
+      link: "https://www.swiggy.com/collections/83634?collection_id=83634&search_context=southindian&tags=layout_CCS_SouthIndian&type=rcv2",
+      text: "South Indian",
+      type: "WEBLINK",
+    },
+    entityType: "BANNER",
+    accessibility: {
+      altText: "restaurants curated for south indian",
+      altTextCta: "open",
+    },
+    entityId:
+      "swiggy://collectionV2?collection_id=83634&tags=layout_CCS_SouthIndian&search_context=southindian",
+    frequencyCapping: {},
+    externalMarketing: {},
+  },
+];
